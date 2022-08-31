864d55daf1083d6497b6e284d4ebd84b7b50cbf8
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-01 00:21:19 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,17 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let num=result.input.split(',');//9,5,-7,-7,5,-9,-4
   
+let count=0;
+
+for (let i = 0; i < num.length; i=i+2) {
+    const element = parseInt(num[i+2]);
+    const element2=parseInt(num[i+4])
+    if (element>element2) {
+        count++;
+    }
+}
+console.log(count);
+
 });
