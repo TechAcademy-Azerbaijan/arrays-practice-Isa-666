8d6366c379cab3141975ea923fbb22352ea0fef5
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-01 21:20:45 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let num = result.input.split(',') //0,1,-2,1,0,0,3
+
+    let res = num.filter((x, i) => num.indexOf(x) === i);
+
+    console.log(res.join('\n'));
   
 });
