9ff43dc5056c092eaf835e0df5a6aed5c1fb8ba3
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-03 16:49:27 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,28 +3,29 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-     let num = result.input.split(',')//3,5,-7,7,5,-9,-4
 
-let count=2
+    let num = result.input.split(',')//3,5,-7,7,5,-9,-4
+
     let max = parseInt(num[0]);
     let min = parseInt(num[0]);
+let max_index=0
+let min_index=0
 
     for (let i = 0; i < num.length-1; i++) {
         let element1 = parseInt(num[i]);
         if (element1 > max) {
             max = element1;
+            max_index=i
         }
-        for (let j = 0; j < num.length-1; j++) {
-            const element2 = parseInt(num[j]);
-            if (element2 < min) {
-                min = element2
+            if (element1 < min) {
+                min = element1
+                min_index=i
             }
-        }
+        
 }
-[num[3],num[5]]=[num[5],num[3]]
-//[num[min],num[max]]=[num[max],num[min]]
+[num[max_index],num[min_index]]=[num[min_index],num[max_index]]
+
   
-    console.log(num.join('\n'));
+    console.log(num.join(`\n`));
   
 });
