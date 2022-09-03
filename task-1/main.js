0b9793c7ecd4da4316adbe7068d083bb38dee2ba
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-03 16:26:30 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,35 +3,29 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-      let num = result.input.split(',')
-    let count = 0;
+ 
+      let num = result.input.split(',') //1,6,2,6,3
+    
     let sum = 0;
-    let max = parseInt(num[0]);
-    let max2 = parseInt(num[0])
-    for (let i = 0; i < num.length; i++) {
-        let element = parseInt(num[i]);
-        if (element > max) {
-            max = element;
+    let index=num.length;
+    let sum_division;
 
+for (let i = 0; i < num.length; i++) {
+    const element = parseInt(num[i]);
+    sum+=element
+}
+sum_division=parseInt(sum/index)
 
-        }
+let sum_second=0;
+  let count=0;
 
+  for (let j = 0; j < num.length; j++) {
+    const element2 = parseInt(num[j]);
+    if (element2>sum_division) {
+       sum_second+=element2
+        count++
     }
-    for (let j = 0; j < num.length; j++) {
-        const element2 = parseInt(num[j]);
-        if (element2 === max) {
-            count++;
-        }
-    }
-    for (let j = 0; j < num.length; j++) {
-        let element3 = parseInt(num[j]);
-        if (element3 > max2 && element3 < max || element3 === max) {
-            max2 = element3
-        }
-    }
-    sum = max + max2;
-    console.log(sum);
-    console.log(count);
-  
+  }
+  console.log(sum_second);
+  console.log(count);
 });
