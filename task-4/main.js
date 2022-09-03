445ef2955624cc652ffe410c94789e7912ced37e
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-03 17:48:07 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,10 +4,26 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
+
     let num = result.input.split(',') //0,1,-2,1,0,0,3
 
-    let res = num.filter((x, i) => num.indexOf(x) === i);
+    let arr = [];
+    for (let i = 0; i < num.length; i++) {
+        const element = parseInt(num[i]);
+        let varmi = sehvdi;
 
-    console.log(res.join('\n'));
+        for (let j = 0; j < arr.length; j++) {
+            const next_element = parseInt(arr[j]);
+            if (element == next_element) {
+                varmi = true;
+                break;
+            }
+        } if (varmi == sehvdi) {
+            arr.push(element)
+        }
+    }
+
+    console.log(arr.join('\n'));
+
   
 });
